b10c09c058e4d4f98bcc98f8ba210f6538fd9d16
ww1516123 <ww1516123@126.com>
2019-07-06 20:24:43 +0800

    support csv

--- parser/csv.go
+++ parser/csv.go
@@ -1,17 +1,31 @@
 package parser
+
 import (
+	"strings"
+
 	"github.com/housepower/clickhouse_sinker/model"
+	"github.com/tidwall/gjson"
+	"github.com/wswz/go_commons/log"
 )
+
 type CsvParser struct {
-  title []string
-  delimiter string
-  mp map[string]interface{}
+	title     []string
+	delimiter string
 }
 
 func (c *CsvParser) Parse(bs []byte) model.Metric {
-  msgData := string(bs)
-  msgs=strings.Split("msgData", c.delimiter)
-	return &CsvMetric{}
+	msgData := string(bs)
+	msgs := strings.Split(msgData, c.delimiter)
+	v := make(map[string]interface{})
+	for i, key := range c.title {
+		v[key] = msgs[i]
+	}
+	b, err := json.Marshal(v)
+	if err != nil {
+		log.Infof("the csv value: %s , parse json failed", v)
+		return nil
+	}
+	return &CsvMetric{string(b)}
 }
 
 type CsvMetric struct {
