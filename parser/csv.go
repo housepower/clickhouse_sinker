564658b0202c045d399d9eb629f40ff399f58e93
MapleRan <ww1516123@126.com>
2019-07-18 18:07:55 +0800

    modify json to map

--- parser/csv.go
+++ parser/csv.go
@@ -2,10 +2,8 @@ package parser
 
 import (
 	"strings"
-
+	"strconv"
 	"github.com/housepower/clickhouse_sinker/model"
-	"github.com/tidwall/gjson"
-	"github.com/wswz/go_commons/log"
 )
 
 type CsvParser struct {
@@ -16,34 +14,34 @@ type CsvParser struct {
 func (c *CsvParser) Parse(bs []byte) model.Metric {
 	msgData := string(bs)
 	msgs := strings.Split(msgData, c.delimiter)
-	v := make(map[string]interface{})
+	v := make(map[string]string)
 	for i, key := range c.title {
 		v[key] = msgs[i]
 	}
-	b, err := json.Marshal(v)
-	if err != nil {
-		log.Infof("the csv value: %s , parse json failed", v)
-		return nil
-	}
-	return &CsvMetric{string(b)}
+	return &CsvMetric{v}
 }
 
 type CsvMetric struct {
-	raw string
+	mp map[string]string
 }
 
 func (c *CsvMetric) Get(key string) interface{} {
-	return gjson.Get(c.raw, key).Value()
+	return c.mp[key]
 }
 
 func (c *CsvMetric) GetString(key string) string {
-	return gjson.Get(c.raw, key).String()
+	v,_:= c.mp[key]
+	return v
 }
 
 func (c *CsvMetric) GetFloat(key string) float64 {
-	return gjson.Get(c.raw, key).Float()
+	v,_:= c.mp[key]
+	n, _ := strconv.ParseFloat(v,64);
+	return n
 }
 
 func (c *CsvMetric) GetInt(key string) int64 {
-	return gjson.Get(c.raw, key).Int()
+	v,_:= c.mp[key]
+	n, _ :=strconv.ParseInt(v, 10, 64)
+	return n
 }
