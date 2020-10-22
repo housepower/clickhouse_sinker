c6f808a5cb962526ad1b14829ee911cb01024ee3
Artem Chekunov <achekunov@libertyglobal.com>
2020-10-22 14:22:39 +0200

    master: docker-entrypoint-initdb.d/logstash.sh: add extra nullable fields

--- docker-entrypoint-initdb.d/logstash.sh
+++ docker-entrypoint-initdb.d/logstash.sh
@@ -5,10 +5,18 @@ set -e
 clickhouse client -n <<-EOSQL
     USE default;
     CREATE TABLE IF NOT EXISTS logstash (
-        date Date,
-        level String,
-        message String
-    ) ENGINE = SummingMergeTree(date, (date, level, message), 8192);
+      date Date,
+      date_nullable Nullable(Date),
+      level String,
+      message String,
+      str String,
+      str_nullable Nullable(String),
+      num Int64,
+      num_nullable Nullable(Int64),
+      fnum Float64,
+      fnum_nullable Nullable(Float64)
+    )
+    ENGINE = SummingMergeTree(date, (date, level, message), 8192);
 
 
 EOSQL
