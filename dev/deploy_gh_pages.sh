78155c03f0fcc5ca39efa2582fc8b7570e17d32b
sundy-li <543950155@qq.com>
2020-12-19 13:14:08 +0000

    Update Token

--- dev/deploy_gh_pages.sh
+++ dev/deploy_gh_pages.sh
@@ -8,4 +8,4 @@ git config --global user.email 'dummy@dummy.dummy'
 git init
 git add -A
 git commit -m 'Deploy GitHub Pages'
-git push -f https://pan3793:${PAGES_DEPLOY_TOKEN}@github.com/housepower/clickhouse_sinker.git master:gh-pages
+git push -f https://sundy-li:${SINKER_DEPLOY_KEY}@github.com/housepower/clickhouse_sinker.git master:gh-pages
