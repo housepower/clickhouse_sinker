d4e0074f91a2e9d3aed66e55a5db9a2c08ca4569
sundy-li <543950155@qq.com>
2020-12-19 13:26:51 +0000

    Update users

--- dev/deploy_gh_pages.sh
+++ dev/deploy_gh_pages.sh
@@ -8,4 +8,4 @@ git config --global user.email 'dummy@dummy.dummy'
 git init
 git add -A
 git commit -m 'Deploy GitHub Pages'
-git push -f https://sundy:${SINKER_DEPLOY_KEY}@github.com/housepower/clickhouse_sinker.git master:gh-pages
+git push -f https://sundy-li:${SINKER_DEPLOY_KEY}@github.com/housepower/clickhouse_sinker.git master:gh-pages
