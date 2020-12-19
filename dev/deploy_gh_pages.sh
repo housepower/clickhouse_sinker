5c83e91f93753aa1136d0a5496ebd79caa45d361
sundy-li <543950155@qq.com>
2020-12-19 13:17:39 +0000

    Update Token

--- dev/deploy_gh_pages.sh
+++ dev/deploy_gh_pages.sh
@@ -2,10 +2,10 @@
 
 cd docs/.vuepress/dist
 
-git config --global user.name  'GitHub Workflow'
+git config --global user.name  'sundy'
 git config --global user.email 'dummy@dummy.dummy'
 
 git init
 git add -A
 git commit -m 'Deploy GitHub Pages'
-git push -f https://sundy-li:${SINKER_DEPLOY_KEY}@github.com/housepower/clickhouse_sinker.git master:gh-pages
+git push -f https://sundy:${SINKER_DEPLOY_KEY}@github.com/housepower/clickhouse_sinker.git master:gh-pages
