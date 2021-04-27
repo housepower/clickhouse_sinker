housepower/clickhouse_sinker

commit 104123bb018a441cd25683b9bf5cf7ec2471203d

parser treats type mismatch as error

model/message.go
@@ -170,8 +170,9 @@ func PutRow(r *Row) {
 	rowPool.Put(r)
 }
 
-func MetricToRow(metric Metric, msg InputMessage, dims []*ColumnWithType) (row *Row) {
+func MetricToRow(metric Metric, msg InputMessage, dims []*ColumnWithType) (row *Row, err error) {
 	row = GetRow()
+	var val interface{}
 	for _, dim := range dims {
 		if strings.HasPrefix(dim.Name, "__kafka") {
 			if strings.HasSuffix(dim.Name, "_topic") {
@@ -182,7 +183,11 @@ func MetricToRow(metric Metric, msg InputMessage, dims []*ColumnWithType) (row *
 				*row = append(*row, msg.Offset)
 			}
 		} else {
-			*row = append(*row, GetValueByType(metric, dim))
+			if val, err = GetValueByType(metric, dim); err != nil {
+				PutRow(row)
+				return
+			}
+			*row = append(*row, val)
 		}
 	}
 	return

task/task.go
@@ -19,7 +19,6 @@ import (
 	"context"
 	"fmt"
 	"math"
-	"os"
 	"runtime"
 	"strconv"
 	"strings"
@@ -229,23 +228,27 @@ func (service *Service) put(msg model.InputMessage) {
 	_ = util.GlobalParsingPool.Submit(func() {
 		var row *model.Row
 		var foundNewKeys bool
+		var metric model.Metric
 		p := service.pp.Get()
-		metric, err := p.Parse(msg.Value)
+		metric, err = p.Parse(msg.Value)
+		if err == nil {
+			row, err = model.MetricToRow(metric, msg, service.dims)
+		}
 		if err != nil {
 			statistics.ParseMsgsErrorTotal.WithLabelValues(taskCfg.Name).Inc()
 			if service.limiter1.Allow() {
 				util.Logger.Errorf("%s: failed to parse message(topic %v, partition %d, offset %v) %+v, string(value) <<<%+v>>>, got error %+v",
 					service.cfg.Task.Name, msg.Topic, msg.Partition, msg.Offset, msg, string(msg.Value), err)
 			}
-		} else {
-			row = model.MetricToRow(metric, msg, service.dims)
+			// WARNNING: metric.GetXXX may depend on p. Don't call them after p been freed.
+			service.pp.Put(p)
+			return
 		}
+		service.pp.Put(p)
+
 		if taskCfg.DynamicSchema.Enable {
 			foundNewKeys = metric.GetNewKeys(&service.knownKeys, &service.newKeys)
 		}
-		// WARNNING: metric.GetXXX may depend on p. Don't call them after p been freed.
-		service.pp.Put(p)
-
 		if foundNewKeys {
 			cntNewKeys := atomic.AddInt32(&service.cntNewKeys, 1)
 			if cntNewKeys == 1 {
@@ -261,7 +264,6 @@ func (service *Service) put(msg model.InputMessage) {
 				}
 				if service.tid, err = util.GlobalTimerWheel.Schedule(time.Duration(taskCfg.FlushInterval)*time.Second, service.changeSchema, nil); err != nil {
 					util.Logger.Fatalf("got error %+v", err)
-					os.Exit(-1)
 				}
 			}
 		}
@@ -289,13 +291,11 @@ func (service *Service) changeSchema(arg interface{}) {
 	// change schema
 	if err = service.clickhouse.ChangeSchema(&service.newKeys); err != nil {
 		util.Logger.Fatalf("%s: clickhouse.ChangeSchema failed with error: %+v", taskCfg.Name, err)
-		os.Exit(-1)
 	}
 	// restart myself
 	service.Stop()
 	if err = service.Init(); err != nil {
 		util.Logger.Fatalf("%s: init failed with error: %+v", taskCfg.Name, err)
-		os.Exit(-1)
 	}
 	go service.Run(service.parentCtx)
 }

model/metric.go
@@ -15,19 +15,20 @@ limitations under the License.
 
 package model
 
-import "sync"
+import (
+	"sync"
+)
 
 // Metric interface for metric collection
 type Metric interface {
-	Get(key string) interface{}
-	GetString(key string, nullable bool) interface{}
-	GetArray(key string, t string) interface{}
-	GetFloat(key string, nullable bool) interface{}
-	GetInt(key string, nullable bool) interface{}
-	GetDate(key string, nullable bool) interface{}
-	GetDateTime(key string, nullable bool) interface{}
-	GetDateTime64(key string, nullable bool) interface{}
-	GetElasticDateTime(key string, nullable bool) interface{}
+	GetString(key string, nullable bool) (val interface{}, err error)
+	GetFloat(key string, nullable bool) (val interface{}, err error)
+	GetInt(key string, nullable bool) (val interface{}, err error)
+	GetDate(key string, nullable bool) (val interface{}, err error)
+	GetDateTime(key string, nullable bool) (val interface{}, err error)
+	GetDateTime64(key string, nullable bool) (val interface{}, err error)
+	GetElasticDateTime(key string, nullable bool) (val interface{}, err error)
+	GetArray(key string, t string) (val interface{}, err error)
 	GetNewKeys(knownKeys *sync.Map, newKeys *sync.Map) bool
 }
 

model/value.go
@@ -16,8 +16,6 @@ package model
 
 import (
 	"strings"
-
-	"github.com/ClickHouse/clickhouse-go"
 )
 
 const (
@@ -43,7 +41,7 @@ var (
 )
 
 // There are only three cases for the value type of metric, (float64, string, map [string] interface {})
-func GetValueByType(metric Metric, cwt *ColumnWithType) interface{} {
+func GetValueByType(metric Metric, cwt *ColumnWithType) (val interface{}, err error) {
 	name := cwt.SourceName
 	switch cwt.Type {
 	case Int:
@@ -53,11 +51,11 @@ func GetValueByType(metric Metric, cwt *ColumnWithType) interface{} {
 	case String:
 		return metric.GetString(name, cwt.Nullable)
 	case IntArray:
-		return clickhouse.Array(metric.GetArray(name, "int"))
+		return metric.GetArray(name, "int")
 	case FloatArray:
-		return clickhouse.Array(metric.GetArray(name, "float"))
+		return metric.GetArray(name, "float")
 	case StringArray:
-		return clickhouse.Array(metric.GetArray(name, "string"))
+		return metric.GetArray(name, "string")
 	case Date:
 		return metric.GetDate(name, cwt.Nullable)
 	case DateTime:

output/clickhouse.go
@@ -22,7 +22,6 @@ import (
 	"fmt"
 	"io"
 	"math"
-	"os"
 	"regexp"
 	"strings"
 	"sync"
@@ -154,7 +153,7 @@ func (c *ClickHouse) loopWrite(batch *model.Batch) {
 		if shouldReconnect(err) && (c.cfg.Clickhouse.RetryTimes <= 0 || times < c.cfg.Clickhouse.RetryTimes) {
 			time.Sleep(10 * time.Second)
 		} else {
-			os.Exit(-1)
+			util.Logger.Fatalf("%s: ClickHouse.loopWrite failed", c.cfg.Task.Name)
 		}
 	}
 }

parser/csv.go
@@ -17,6 +17,7 @@ package parser
 import (
 	"bytes"
 	"encoding/csv"
+	"io"
 	"strconv"
 	"strings"
 	"sync"
@@ -55,125 +56,142 @@ type CsvMetric struct {
 	values []string
 }
 
-// Get returns the value corresponding to a column expects called
-// interpret the type
-func (c *CsvMetric) Get(key string) interface{} {
-	for i, k := range c.pp.csvFormat {
-		if k == key && i < len(c.values) {
-			return c.values[i]
-		}
-	}
-	return nil
-}
-
 // GetString get the value as string
-func (c *CsvMetric) GetString(key string, nullable bool) interface{} {
-	_ = nullable // nullable can not be supported with csv
-	for i, k := range c.pp.csvFormat {
-		if k == key && i < len(c.values) {
-			return c.values[i]
+func (c *CsvMetric) GetString(key string, nullable bool) (val interface{}, err error) {
+	var idx int
+	var ok bool
+	if idx, ok = c.pp.csvFormat[key]; !ok {
+		if nullable {
+			return
 		}
+		val = ""
+		return
 	}
-	return ""
+	val = c.values[idx]
+	return
 }
 
 // GetFloat returns the value as float
-func (c *CsvMetric) GetFloat(key string, nullable bool) interface{} {
-	_ = nullable // nullable can not be supported with csv
-	for i, k := range c.pp.csvFormat {
-		if k == key && i < len(c.values) {
-			n, _ := strconv.ParseFloat(c.values[i], 64)
-			return n
+func (c *CsvMetric) GetFloat(key string, nullable bool) (val interface{}, err error) {
+	var idx int
+	var ok bool
+	if idx, ok = c.pp.csvFormat[key]; !ok {
+		if nullable {
+			return
 		}
+		val = float64(0.0)
+		return
 	}
-	return float64(0)
+	val, err = strconv.ParseFloat(c.values[idx], 64)
+	return
 }
 
 // GetInt returns int
-func (c *CsvMetric) GetInt(key string, nullable bool) interface{} {
-	_ = nullable // nullable can not be supported with csv
-	for i, k := range c.pp.csvFormat {
-		if k == key && i < len(c.values) {
-			n, _ := strconv.ParseInt(c.values[i], 10, 64)
-			return n
+func (c *CsvMetric) GetInt(key string, nullable bool) (val interface{}, err error) {
+	var idx int
+	var ok bool
+	if idx, ok = c.pp.csvFormat[key]; !ok {
+		if nullable {
+			return
 		}
+		val = int64(0)
+		return
 	}
-	return int64(0)
+	val, err = strconv.ParseInt(c.values[idx], 10, 64)
+	return
+}
+
+func (c *CsvMetric) GetDate(key string, nullable bool) (val interface{}, err error) {
+	return c.GetDateTime(key, nullable)
+}
+
+func (c *CsvMetric) GetDateTime(key string, nullable bool) (val interface{}, err error) {
+	var idx int
+	var ok bool
+	if idx, ok = c.pp.csvFormat[key]; !ok {
+		if nullable {
+			return
+		}
+		val = Epoch
+		return
+	}
+	val, err = c.pp.ParseDateTime(key, c.values[idx])
+	return
+}
+
+func (c *CsvMetric) GetDateTime64(key string, nullable bool) (val interface{}, err error) {
+	return c.GetDateTime(key, nullable)
+}
+
+func (c *CsvMetric) GetElasticDateTime(key string, nullable bool) (val interface{}, err error) {
+	var t interface{}
+	if t, err = c.GetDateTime(key, nullable); err != nil {
+		return
+	}
+	if t != nil {
+		val = t.(time.Time).Unix()
+	}
+	return
 }
 
 // GetArray parse an CSV encoded array
-func (c *CsvMetric) GetArray(key string, t string) interface{} {
-	var err error
+func (c *CsvMetric) GetArray(key string, t string) (val interface{}, err error) {
+	var s interface{}
 	var array []string
 	var r *csv.Reader
-	val, _ := c.GetString(key, false).(string)
-	valLen := len(val)
-	if val == "" || val[0] != '[' || val[valLen-1] != ']' {
-		goto QUIT
+	if s, err = c.GetString(key, false); err != nil {
+		return
 	}
-	r = csv.NewReader(strings.NewReader(val[1 : valLen-1]))
+	str, _ := s.(string)
+	strLen := len(str)
+	if str == "" || str[0] != '[' || str[strLen-1] != ']' {
+		err = errors.Errorf("GetArray %s got unexpected value %s", key, str)
+		return
+	}
+	r = csv.NewReader(strings.NewReader(str[1 : strLen-1]))
 	if array, err = r.Read(); err != nil {
-		goto QUIT
+		if errors.Is(err, io.EOF) {
+			err = nil
+			switch t {
+			case "int":
+				val = []int64{}
+			case "float":
+				val = []float64{}
+			case "string":
+				val = []string{}
+			default:
+				panic("LOGIC ERROR: not supported array type " + t)
+			}
+		}
+		return
 	}
 	switch t {
 	case "int":
 		results := make([]int64, 0, len(array))
+		var v int64
 		for _, e := range array {
-			v, _ := strconv.ParseInt(e, 10, 64)
+			if v, err = strconv.ParseInt(e, 10, 64); err != nil {
+				return
+			}
 			results = append(results, v)
 		}
-		return results
+		val = results
 	case "float":
 		results := make([]float64, 0, len(array))
+		var v float64
 		for _, e := range array {
-			v, _ := strconv.ParseFloat(e, 64)
+			if v, err = strconv.ParseFloat(e, 64); err != nil {
+				return
+			}
 			results = append(results, v)
 		}
-		return results
+		val = results
 	case "string":
-		return array
+		val = array
 	default:
-		panic("not supported array type " + t)
+		panic("LOGIC ERROR: not supported array type " + t)
 	}
-QUIT:
-	switch t {
-	case "int":
-		return []int64{}
-	case "float":
-		return []float64{}
-	case "string":
-		return []string{}
-	default:
-		return nil
-	}
-}
-
-func (c *CsvMetric) GetDate(key string, nullable bool) interface{} {
-	_ = nullable // nullable can not be supported with csv
-
-	val, _ := c.GetString(key, false).(string)
-	t, _ := c.pp.ParseDateTime(key, val)
-	return t
-}
-
-func (c *CsvMetric) GetDateTime(key string, nullable bool) interface{} {
-	val, _ := c.GetString(key, false).(string)
-	t, _ := c.pp.ParseDateTime(key, val)
-	return t
-}
-
-func (c *CsvMetric) GetDateTime64(key string, nullable bool) interface{} {
-	val, _ := c.GetString(key, false).(string)
-	t, _ := c.pp.ParseDateTime(key, val)
-	return t
-}
-
-func (c *CsvMetric) GetElasticDateTime(key string, nullable bool) interface{} {
-	_ = nullable // nullable can not be supported with csv
-	val, _ := c.GetString(key, false).(string)
-	t, _ := time.Parse(time.RFC3339, val)
-
-	return t.Unix()
+	return
 }
 
 func (c *CsvMetric) GetNewKeys(knownKeys *sync.Map, newKeys *sync.Map) bool {

parser/csv_test.go
@@ -1,199 +1,0 @@
-/*Copyright [2019] housepower
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-package parser
-
-import (
-	"testing"
-	"time"
-
-	"github.com/stretchr/testify/require"
-)
-
-func TestCsvInt(t *testing.T) {
-	pp, _ := NewParserPool("csv", csvSampleSchema, "", "")
-	parser := pp.Get()
-	defer pp.Put(parser)
-	metric, err := parser.Parse(csvSample)
-	require.Nil(t, err)
-
-	var exp, act int64
-	exp = 1536813227
-	act, _ = metric.GetInt("its", false).(int64)
-	require.Equal(t, exp, act)
-
-	exp = 0
-	act, _ = metric.GetInt("not_exist", false).(int64)
-	require.Equal(t, exp, act)
-
-	act, _ = metric.GetInt("not_exist", true).(int64)
-	require.Equal(t, exp, act)
-}
-
-func TestCsvFloat(t *testing.T) {
-	pp, _ := NewParserPool("csv", csvSampleSchema, "", "")
-	parser := pp.Get()
-	defer pp.Put(parser)
-	metric, err := parser.Parse(csvSample)
-	require.Nil(t, err)
-
-	var exp, act float64
-	exp = 0.11
-	act, _ = metric.GetFloat("percent", false).(float64)
-	require.Equal(t, exp, act)
-
-	exp = 0.0
-	act, _ = metric.GetFloat("not_exist", false).(float64)
-	require.Equal(t, exp, act)
-
-	act, _ = metric.GetFloat("not_exist", true).(float64)
-	require.Equal(t, exp, act)
-}
-
-func TestCsvString(t *testing.T) {
-	pp, _ := NewParserPool("csv", csvSampleSchema, "", "")
-	parser := pp.Get()
-	defer pp.Put(parser)
-	metric, err := parser.Parse(csvSample)
-	require.Nil(t, err)
-
-	var exp, act string
-	exp = `escaped_"ws`
-	act, _ = metric.GetString("channel", false).(string)
-	require.Equal(t, exp, act)
-
-	exp = ""
-	act, _ = metric.GetString("not_exist", false).(string)
-	require.Equal(t, exp, act)
-
-	act, _ = metric.GetString("not_exist", true).(string)
-	require.Equal(t, exp, act)
-}
-
-func TestCsvDate(t *testing.T) {
-	pp, _ := NewParserPool("csv", csvSampleSchema, "", "")
-	parser := pp.Get()
-	defer pp.Put(parser)
-	metric, err := parser.Parse(csvSample)
-	require.Nil(t, err)
-
-	var exp, act time.Time
-	exp = time.Date(2019, 12, 16, 0, 0, 0, 0, time.Local)
-	act, _ = metric.GetDate("date1", false).(time.Time)
-	require.Equal(t, exp, act)
-}
-
-func TestCsvDateTime(t *testing.T) {
-	pp, _ := NewParserPool("csv", csvSampleSchema, "", "")
-	parser := pp.Get()
-	defer pp.Put(parser)
-	metric, err := parser.Parse(csvSample)
-	require.Nil(t, err)
-
-	var exp, act time.Time
-	exp = time.Date(2019, 12, 16, 12, 10, 30, 0, time.UTC)
-	act, _ = metric.GetDateTime("time_sec_rfc3339_1", false).(time.Time)
-	require.Equal(t, exp, act)
-
-	exp = time.Date(2019, 12, 16, 12, 10, 30, 0, time.FixedZone("CST", 8*60*60)).In(time.UTC)
-	act = metric.GetDateTime("time_sec_rfc3339_2", false).(time.Time).In(time.UTC)
-	require.Equal(t, exp, act)
-
-	exp = time.Date(2019, 12, 16, 12, 10, 30, 0, time.Local).In(time.UTC)
-	act = metric.GetDateTime("time_sec_clickhouse_1", false).(time.Time).In(time.UTC)
-	require.Equal(t, exp, act)
-
-	exp = time.Time{}
-	act, _ = metric.GetDateTime("not_exist", false).(time.Time)
-	require.Equal(t, exp, act)
-}
-
-func TestCsvDateTime64(t *testing.T) {
-	pp, _ := NewParserPool("csv", csvSampleSchema, "", "")
-	parser := pp.Get()
-	defer pp.Put(parser)
-	metric, err := parser.Parse(csvSample)
-	require.Nil(t, err)
-
-	var exp, act time.Time
-	exp = time.Date(2019, 12, 16, 12, 10, 30, 123000000, time.UTC)
-	act, _ = metric.GetDateTime64("time_ms_rfc3339_1", false).(time.Time)
-	require.Equal(t, exp, act)
-
-	exp = time.Date(2019, 12, 16, 12, 10, 30, 123000000, time.FixedZone("CST", 8*60*60)).In(time.UTC)
-	act = metric.GetDateTime64("time_ms_rfc3339_2", false).(time.Time).In(time.UTC)
-	require.EqualValues(t, exp, act)
-
-	exp = time.Date(2019, 12, 16, 12, 10, 30, 123000000, time.Local).In(time.UTC)
-	act = metric.GetDateTime64("time_ms_clickhouse_1", false).(time.Time).In(time.UTC)
-	require.Equal(t, exp, act)
-
-	exp = time.Time{}
-	act, _ = metric.GetDateTime64("not_exist", false).(time.Time)
-	require.Equal(t, exp, act)
-}
-
-func TestCsvElasticDateTime(t *testing.T) {
-	pp, _ := NewParserPool("csv", csvSampleSchema, "", "")
-	parser := pp.Get()
-	defer pp.Put(parser)
-	metric, err := parser.Parse(csvSample)
-	require.Nil(t, err)
-
-	var exp, act int64
-	// {"date": "2019-12-16T12:10:30Z"}
-	// TZ=UTC date -d @1576498230 => Mon 16 Dec 2019 12:10:30 PM UTC
-	exp = 1576498230
-	act, _ = metric.GetElasticDateTime("time_sec_rfc3339_1", false).(int64)
-	require.Equal(t, exp, act)
-
-	exp = -62135596800
-	act, _ = metric.GetElasticDateTime("not_exist", false).(int64)
-	require.Equal(t, exp, act)
-
-	act, _ = metric.GetElasticDateTime("not_exist", true).(int64)
-	require.Equal(t, exp, act)
-}
-
-func TestCsvArray(t *testing.T) {
-	pp, _ := NewParserPool("csv", csvSampleSchema, "", "")
-	parser := pp.Get()
-	defer pp.Put(parser)
-	metric, err := parser.Parse(csvSample)
-	require.Nil(t, err)
-
-	actI, _ := metric.GetArray("array_int", "int").([]int64)
-	expI := []int64{1, 2, 3}
-	require.Equal(t, expI, actI)
-
-	actF, _ := metric.GetArray("array_float", "float").([]float64)
-	expF := []float64{1.1, 2.2, 3.3}
-	require.Equal(t, expF, actF)
-
-	actS, _ := metric.GetArray("array_string", "string").([]string)
-	expS := []string{"aa", "bb", "cc"}
-	require.Equal(t, expS, actS)
-
-	actIE, _ := metric.GetArray("array_empty", "int").([]int64)
-	expIE := []int64{}
-	require.Equal(t, expIE, actIE)
-
-	actFE, _ := metric.GetArray("array_empty", "float").([]float64)
-	expFE := []float64{}
-	require.Equal(t, expFE, actFE)
-
-	actSE, _ := metric.GetArray("array_empty", "string").([]string)
-	expSE := []string{}
-	require.Equal(t, expSE, actSE)
-}

parser/dummy.go
@@ -1,69 +1,0 @@
-/*Copyright [2019] housepower
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-package parser
-
-import (
-	"sync"
-	"time"
-)
-
-type DummyMetric struct {
-}
-
-func (c *DummyMetric) Get(key string) interface{} {
-	return ""
-}
-
-func (c *DummyMetric) GetString(key string) string {
-	return ""
-}
-
-func (c *DummyMetric) GetFloat(key string) float64 {
-	return 0
-}
-
-func (c *DummyMetric) GetInt(key string) int64 {
-	return 0
-}
-
-// GetArray is Empty implemented for DummyMetric
-func (c *DummyMetric) GetArray(key string, t string) interface{} {
-	return []string{}
-}
-
-func (c *DummyMetric) String() string {
-	return "_dummy"
-}
-
-func (c *DummyMetric) GetDate(key string) (t time.Time) {
-	return
-}
-
-func (c *DummyMetric) GetDateTime(key string) (t time.Time) {
-	return
-}
-
-func (c *DummyMetric) GetDateTime64(key string) (t time.Time) {
-	return
-}
-
-func (c *DummyMetric) GetElasticDateTime(key string) int64 {
-	return 0
-}
-
-func (c *DummyMetric) GetNewKeys(knownKeys *sync.Map, newKeys *sync.Map) bool {
-	return false
-}

commit bada22274d1b9c648aae2e9ec0ed1d1c7c58214c

Always PutElem even if there's parsing error. Fixed bug introduced by 104123bb.

model/message.go
@@ -185,6 +185,7 @@ func MetricToRow(metric Metric, msg InputMessage, dims []*ColumnWithType) (row *
 		} else {
 			if val, err = GetValueByType(metric, dim); err != nil {
 				PutRow(row)
+				row = nil
 				return
 			}
 			*row = append(*row, val)

task/task.go
@@ -235,16 +235,15 @@ func (service *Service) put(msg model.InputMessage) {
 		if err == nil {
 			row, err = model.MetricToRow(metric, msg, service.dims)
 		}
+		// WARNNING: Always PutElem even if there's parsing error, so that this message can be acked to Kafka and skipped writting to ClickHouse.
 		if err != nil {
 			statistics.ParseMsgsErrorTotal.WithLabelValues(taskCfg.Name).Inc()
 			if service.limiter1.Allow() {
 				util.Logger.Error(fmt.Sprintf("failed to parse message(topic %v, partition %d, offset %v)",
 					msg.Topic, msg.Partition, msg.Offset), zap.String("message value", string(msg.Value)), zap.String("task", service.cfg.Task.Name), zap.Error(err))
 			}
-			// WARNNING: metric.GetXXX may depend on p. Don't call them after p been freed.
-			service.pp.Put(p)
-			return
 		}
+		// WARNNING: metric.GetXXX may depend on p. Don't call them after p been freed.
 		service.pp.Put(p)
 
 		if taskCfg.DynamicSchema.Enable {