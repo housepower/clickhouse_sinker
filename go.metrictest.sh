09b6a3eae8a668e3199000262589b7f4c79170c8
YenchangChan <chenyanchang1990@163.com>
2024-01-19 13:35:26 +0800

    make systest issue

--- go.metrictest.sh
+++ go.metrictest.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 echo "create database"
-curl "localhost:8123" -d "CREATE DATABASE IF NOT EXISTS gauge ON CLUSTER abc"
+curl "localhost:58123" -d "CREATE DATABASE IF NOT EXISTS gauge ON CLUSTER abc"
 
 echo "create metric test tables"
-curl "localhost:8123" -d "DROP TABLE IF EXISTS test_prom_metric ON CLUSTER abc SYNC"
-curl "localhost:8123" -d "CREATE TABLE test_prom_metric ON CLUSTER abc
+curl "localhost:58123" -d "DROP TABLE IF EXISTS test_prom_metric ON CLUSTER abc SYNC"
+curl "localhost:58123" -d "CREATE TABLE test_prom_metric ON CLUSTER abc
 (
     __series_id__ Int64,
     timestamp DateTime CODEC(DoubleDelta, LZ4),
@@ -14,11 +14,11 @@ curl "localhost:8123" -d "CREATE TABLE test_prom_metric ON CLUSTER abc
 PARTITION BY toYYYYMMDD(timestamp)
 ORDER BY (__series_id__, timestamp);"
 
-curl "localhost:8123" -d "DROP TABLE IF EXISTS dist_test_prom_metric ON CLUSTER abc SYNC"
-curl "localhost:8123" -d "CREATE TABLE dist_test_prom_metric ON CLUSTER abc AS test_prom_metric ENGINE = Distributed(abc, default, test_prom_metric);"
+curl "localhost:58123" -d "DROP TABLE IF EXISTS dist_test_prom_metric ON CLUSTER abc SYNC"
+curl "localhost:58123" -d "CREATE TABLE dist_test_prom_metric ON CLUSTER abc AS test_prom_metric ENGINE = Distributed(abc, default, test_prom_metric);"
 
-curl "localhost:8123" -d "DROP TABLE IF EXISTS test_prom_series ON CLUSTER abc SYNC"
-curl "localhost:8123" -d "CREATE TABLE test_prom_series ON CLUSTER abc
+curl "localhost:58123" -d "DROP TABLE IF EXISTS test_prom_series ON CLUSTER abc SYNC"
+curl "localhost:58123" -d "CREATE TABLE test_prom_series ON CLUSTER abc
 (
     __series_id__ Int64,
     __mgmt_id__ Int64,
@@ -27,8 +27,8 @@ curl "localhost:8123" -d "CREATE TABLE test_prom_series ON CLUSTER abc
 ) ENGINE=ReplicatedReplacingMergeTree()
 ORDER BY (__name__, __series_id__);"
 
-curl "localhost:8123" -d "DROP TABLE IF EXISTS dist_test_prom_series ON CLUSTER abc SYNC"
-curl "localhost:8123" -d "CREATE TABLE dist_test_prom_series ON CLUSTER abc AS test_prom_series ENGINE = Distributed(abc, default, test_prom_series);"
+curl "localhost:58123" -d "DROP TABLE IF EXISTS dist_test_prom_series ON CLUSTER abc SYNC"
+curl "localhost:58123" -d "CREATE TABLE dist_test_prom_series ON CLUSTER abc AS test_prom_series ENGINE = Distributed(abc, default, test_prom_series);"
 
 echo "send messages to kafka"
 echo "cat /tmp/test_prom_metric.data | kafka-console-producer --topic test_metric_topic --broker-list localhost:9092" > send.sh
@@ -41,20 +41,20 @@ sudo docker exec kafka sh /tmp/send.sh
 echo "start clickhouse_sinker to consume"
 timeout 30 ./bin/clickhouse_sinker --local-cfg-file docker/test_prom_metric.hjson
 
-schema=`curl "localhost:8123" -d 'DESC test_prom_metric' 2>/dev/null | sort | tr -d '\t' | tr -d ' '| tr '\n' ','`
+schema=`curl "localhost:58123" -d 'DESC test_prom_metric' 2>/dev/null | sort | tr -d '\t' | tr -d ' '| tr '\n' ','`
 echo "Got test_prom_metric schema => $schema"
 [ $schema = "__series_id__Int64,timestampDateTimeDoubleDelta,LZ4,value1Nullable(Float64),value2Nullable(Int64),value3Nullable(Bool),valueFloat32ZSTD(15)," ] || exit 1
 
 
-schema=`curl "localhost:8123" -d 'DESC test_prom_series' 2>/dev/null | sort | tr -d '\t' | tr -d ' '| tr '\n' ','`
+schema=`curl "localhost:58123" -d 'DESC test_prom_series' 2>/dev/null | sort | tr -d '\t' | tr -d ' '| tr '\n' ','`
 echo "Got test_prom_series schema => $schema"
-[ $schema = "key_0Nullable(String),key_1Nullable(String),key_2Nullable(String),key_4Nullable(String),key_5Nullable(String),key_6Nullable(String),key_7Nullable(String),key_8Nullable(String),key_9Nullable(String),labelsString,__mgmt_idInt64,__name__String,__series_id__Int64," ] || exit 1
+[ $schema = "key_0Nullable(String),key_1Nullable(String),key_2Nullable(String),key_4Nullable(String),key_5Nullable(String),key_6Nullable(String),key_7Nullable(String),key_8Nullable(String),key_9Nullable(String),labelsString,__mgmt_id__Int64,__name__String,__series_id__Int64," ] || exit 1
 
 echo "check result 1"
-count=`curl "localhost:8123" -d 'select count() from dist_test_prom_metric'`
+count=`curl "localhost:58123" -d 'select count() from dist_test_prom_metric'`
 echo "Got test_prom_metric count => $count"
 [ $count -le 10000 ] || exit 1
 
-count=`curl "localhost:8123" -d 'select count() from dist_test_prom_series'`
+count=`curl "localhost:58123" -d 'select count() from dist_test_prom_series'`
 echo "Got test_prom_series count => $count"
 [ $count -eq 1000 ] || exit 1
