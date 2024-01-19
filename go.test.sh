e8069e6722bc2f12a2178ce8fe2ea94dea852879
YenchangChan <chenyanchang1990@163.com>
2024-01-19 13:35:26 +0800

    make systest issue

--- go.test.sh
+++ go.test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "create tables"
-curl "localhost:8123" -d 'DROP TABLE IF EXISTS test_fixed_schema'
-curl "localhost:8123" -d 'CREATE TABLE test_fixed_schema
+curl "localhost:58123" -d 'DROP TABLE IF EXISTS test_fixed_schema'
+curl "localhost:58123" -d 'CREATE TABLE test_fixed_schema
 (
     time DateTime,
     name String,
@@ -13,13 +13,13 @@ ENGINE = MergeTree
 PARTITION BY toYYYYMMDD(time)
 ORDER BY (time, name)'
 
-curl "localhost:8123" -d 'DROP TABLE IF EXISTS test_auto_schema'
-curl "localhost:8123" -d 'CREATE TABLE test_auto_schema AS test_fixed_schema'
+curl "localhost:58123" -d 'DROP TABLE IF EXISTS test_auto_schema'
+curl "localhost:58123" -d 'CREATE TABLE test_auto_schema AS test_fixed_schema'
 
-curl "localhost:8123" -d 'DROP TABLE IF EXISTS test_dynamic_schema'
-curl "localhost:8123" -d 'CREATE TABLE test_dynamic_schema AS test_fixed_schema'
+curl "localhost:58123" -d 'DROP TABLE IF EXISTS test_dynamic_schema'
+curl "localhost:58123" -d 'CREATE TABLE test_dynamic_schema AS test_fixed_schema'
 
-counts=`curl "localhost:8123" -d 'SELECT count() FROM test_fixed_schema UNION ALL SELECT count() FROM test_auto_schema UNION ALL SELECT count() FROM test_dynamic_schema' 2>/dev/null | tr '\n' ','`
+counts=`curl "localhost:58123" -d 'SELECT count() FROM test_fixed_schema UNION ALL SELECT count() FROM test_auto_schema UNION ALL SELECT count() FROM test_dynamic_schema' 2>/dev/null | tr '\n' ','`
 echo "Got initial row counts => $counts"
 [ $counts = "0,0,0," ] || exit 1
 
@@ -54,50 +54,50 @@ timeout 30 ./bin/clickhouse_sinker --local-cfg-file docker/test_auto_schema.hjso
 timeout 60 ./bin/clickhouse_sinker --local-cfg-file docker/test_dynamic_schema.hjson
 
 echo "check result 1"
-count=`curl "localhost:8123" -d 'select count() from test_fixed_schema'`
+count=`curl "localhost:58123" -d 'select count() from test_fixed_schema'`
 echo "Got test_fixed_schema count => $count"
 [ $count -eq 100000 ] || exit 1
 
-count=`curl "localhost:8123" -d 'select count() from test_auto_schema'`
+count=`curl "localhost:58123" -d 'select count() from test_auto_schema'`
 echo "Got test_auto_schema count => $count"
 [ $count -eq 100000 ] || exit 1
 
-schema=`curl "localhost:8123" -d 'DESC test_dynamic_schema' 2>/dev/null | grep newkey | sort | tr -d '\t' | tr '\n' ','`
+schema=`curl "localhost:58123" -d 'DESC test_dynamic_schema' 2>/dev/null | grep newkey | sort | tr -d '\t' | tr '\n' ','`
 echo "Got test_dynamic_schema schema => $schema"
 [ $schema = "newkey00Nullable(Bool),newkey01Nullable(Int64),newkey02Nullable(Float64),newkey03Nullable(String),newkey04Nullable(DateTime64(3))," ] || exit 1
-count=`curl "localhost:8123" -d 'SELECT count() FROM test_dynamic_schema'`
+count=`curl "localhost:58123" -d 'SELECT count() FROM test_dynamic_schema'`
 echo "Got test_dynamic_schema count => $count"
 [ $count -eq 100000 ] || exit 1
 
 echo "truncate tables"
-curl "localhost:8123" -d 'TRUNCATE TABLE test_fixed_schema'
-curl "localhost:8123" -d 'TRUNCATE TABLE test_auto_schema'
-curl "localhost:8123" -d 'TRUNCATE TABLE test_dynamic_schema'
+curl "localhost:58123" -d 'TRUNCATE TABLE test_fixed_schema'
+curl "localhost:58123" -d 'TRUNCATE TABLE test_auto_schema'
+curl "localhost:58123" -d 'TRUNCATE TABLE test_dynamic_schema'
 
 echo "publish clickhouse_sinker config"
-./bin/nacos_publish_config --nacos-addr 127.0.0.1:8848 --nacos-username nacos --nacos-password nacos  --nacos-dataid test_fixed_schema --local-cfg-file docker/test_fixed_schema.hjson
-./bin/nacos_publish_config --nacos-addr 127.0.0.1:8848 --nacos-username nacos --nacos-password nacos  --nacos-dataid test_auto_schema --local-cfg-file docker/test_auto_schema.hjson
-./bin/nacos_publish_config --nacos-addr 127.0.0.1:8848 --nacos-username nacos --nacos-password nacos  --nacos-dataid test_dynamic_schema --local-cfg-file docker/test_dynamic_schema.hjson
+./bin/nacos_publish_config --nacos-addr 127.0.0.1:58848 --nacos-username nacos --nacos-password nacos  --nacos-dataid test_fixed_schema --local-cfg-file docker/test_fixed_schema.hjson
+./bin/nacos_publish_config --nacos-addr 127.0.0.1:58848 --nacos-username nacos --nacos-password nacos  --nacos-dataid test_auto_schema --local-cfg-file docker/test_auto_schema.hjson
+./bin/nacos_publish_config --nacos-addr 127.0.0.1:58848 --nacos-username nacos --nacos-password nacos  --nacos-dataid test_dynamic_schema --local-cfg-file docker/test_dynamic_schema.hjson
 
 echo "start clickhouse_sinker to consume"
 sudo docker exec kafka kafka-consumer-groups --bootstrap-server localhost:9093 --execute --reset-offsets --group test_fixed_schema --all-topics --to-earliest
-timeout 30 ./bin/clickhouse_sinker --nacos-addr 127.0.0.1:8848 --nacos-username nacos --nacos-password nacos --nacos-dataid test_fixed_schema
+timeout 30 ./bin/clickhouse_sinker --nacos-addr 127.0.0.1:58848 --nacos-username nacos --nacos-password nacos --nacos-dataid test_fixed_schema
 
 sudo docker exec kafka kafka-consumer-groups --bootstrap-server localhost:9093 --execute --reset-offsets --group test_auto_schema --all-topics --to-earliest
-timeout 30 ./bin/clickhouse_sinker --nacos-addr 127.0.0.1:8848 --nacos-username nacos --nacos-password nacos --nacos-dataid test_auto_schema
+timeout 30 ./bin/clickhouse_sinker --nacos-addr 127.0.0.1:58848 --nacos-username nacos --nacos-password nacos --nacos-dataid test_auto_schema
 
 sudo docker exec kafka kafka-consumer-groups --bootstrap-server localhost:9093 --execute --reset-offsets --group test_dynamic_schema --all-topics --to-earliest
-timeout 30 ./bin/clickhouse_sinker --nacos-addr 127.0.0.1:8848 --nacos-username nacos --nacos-password nacos --nacos-dataid test_dynamic_schema
+timeout 30 ./bin/clickhouse_sinker --nacos-addr 127.0.0.1:58848 --nacos-username nacos --nacos-password nacos --nacos-dataid test_dynamic_schema
 
 echo "check result 2"
-count=`curl "localhost:8123" -d 'select count() from test_fixed_schema'`
+count=`curl "localhost:58123" -d 'select count() from test_fixed_schema'`
 echo "Got test_fixed_schema count => $count"
 [ $count -eq 100000 ] || exit 1
 
-count=`curl "localhost:8123" -d 'select count() from test_auto_schema'`
+count=`curl "localhost:58123" -d 'select count() from test_auto_schema'`
 echo "Got test_auto_schema count => $count"
 [ $count -eq 100000 ] || exit 1
 
-count=`curl "localhost:8123" -d 'SELECT count() FROM test_dynamic_schema'`
+count=`curl "localhost:58123" -d 'SELECT count() FROM test_dynamic_schema'`
 echo "Got test_dynamic_schema count => $count"
 [ $count -eq 100000 ] || exit 1
